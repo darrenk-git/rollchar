be4030ec02359a4bbe1a9016850f2088eb7fdd8f
darrenk-git <darrenk-git@users.noreply.github.com>
2015-04-04 14:54:38 +0100

    update
    added character load functionality to main rollchar.js

--- displaychar.js
+++ displaychar.js
@@ -14,19 +14,19 @@ module.exports = function(character, callback) {
             
                 if (prop === 'SlugPistol'){
                                       
-                    list.push( ' * ' + prop + ": ");
+                    list.push( '\t\t\t' +prop + ": ");
                     for( var prop2 in obj['SlugPistol'] ) {
                       
                         if( obj[prop].hasOwnProperty( prop2 ) ) {
                           
                           //console.log( prop2 + ": " + prop[prop2]);   
-                          list.push( '   > ' + prop2 + ": " + obj[prop][prop2] );
+                          list.push( '\t\t\t\t> ' + prop2 + ": " + obj[prop][prop2] );
                         
                         }
                     }
                 } else if (prop === 'characteristics') {
                                       
-                    list.push( ' * ' + prop + ": ");
+                    list.push( '   ' + prop + ": ");
                     for( var prop2 in obj['characteristics'] ) {
                       
                         if( obj[prop].hasOwnProperty( prop2 ) ) {
@@ -38,7 +38,7 @@ module.exports = function(character, callback) {
                 } else if ( !(prop instanceof Object) ){
 
                     //console.log( prop + ": " + obj[prop]);
-                    list.push( ' * ' + prop + ": " + obj[prop] );
+                    list.push( '\t\t\t' + prop + ": " + obj[prop] );
 
                    
                 }
@@ -49,31 +49,34 @@ module.exports = function(character, callback) {
     
     }
     
-        
-    console.log(' Name: \t\t' + character.name);
-    console.log(' Biography: \t' + character.biography);
-    console.log(' Gender: \t' + character.gender);
-    console.log(' Race: \t\t' + character.race);
+    console.log('\n------------------------------- Character Sheet --------------------------------');
+    
+    console.log(' \t Name: \t\t' + character.name);
+    console.log(' \t Biography: \t' + character.biography);
+    console.log(' \t Gender: \t' + character.gender);
+    console.log(' \t Race: \t\t' + character.race);
       
     /* String.prototype.capitalize = function () {
         return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
     }
     
     console.log(' Homeworld: \t' + character.homeworld.capitalize()); */
-    console.log(' Homeworld: \t' + character.homeworld);
-    console.log(' Profession: \t' + character.profession);
+    console.log(' \t Homeworld: \t' + character.homeworld);
+    console.log(' \t Profession: \t' + character.profession);
     
-    console.log('\n Skills:');
+    console.log('\n\t Skills: \n');
     var skillsList = logObjectProps(character.skills);
     console.log(skillsList.join('\n').toString());
     
-    console.log('\n Inventory: ');
+    console.log('\n\t Inventory: \n');
     var invList = logObjectProps(character.inventory);
     console.log(invList.join('\n').toString());
   
-    console.log('\n Characteristics: ');
+    console.log('\n\t Characteristics: \n');
     var statList = logObjectProps(character.characteristics);
     console.log(statList.join('\n').toString());
 
+    console.log('\n--------------------------------------------------------------------------------');
+    
     return callback(null, character);
 }
