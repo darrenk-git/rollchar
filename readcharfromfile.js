f6e36fecb566f8fbd8153069fbc8c5394fb8448e
Darren K <darrenk-git@users.noreply.github.com>
2015-04-04 04:44:26 +0100

    init commit

---
+++ readcharfromfile.js
@@ -0,0 +1,39 @@
+//
+//    Loads a Character from a File
+//    This module interacts with the (filesystem / user)  to load the character object data from a json formatted file.
+//    
+//    Takes 3 arguments. Path to file, data to read and a callback
+
+var fs = require('fs');
+var chardata = undefined;
+
+module.exports = function(path, callback) {
+  
+    console.log('---------------Loading character:-------------------\n');
+
+    var fileData = undefined;
+    
+    fs.readFile( 
+        path, 
+        'utf8',
+        handleFile // trigger callback function
+    );
+    
+    // callback function for readFile
+    function handleFile(err, fileData) {
+          
+            if (err) {
+                console.log("Failed to read file:", err);
+            } 
+            
+            //  Parse JSON format file into object
+            chardata = JSON.parse(fileData); 
+
+            // log success to stdout with a little debug test
+            console.log('Successfully loaded stats for ' + chardata['name']);
+            console.log('Biography: ' + chardata['biography']);
+            
+            return callback(null, chardata);
+        }
+        
+}
